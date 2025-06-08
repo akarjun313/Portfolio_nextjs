300a7d502678f3d6b0321f6f75db67feac59b185
Arjun A <137986996+akarjun313@users.noreply.github.com>
2025-06-08 12:11:43 +0530

    Resume link updated

--- src/ui/Navbar.tsx
+++ src/ui/Navbar.tsx
@@ -33,7 +33,7 @@ export default function Navbar() {
 
                 {/* resume button  */}
                 <a
-                    href="https://drive.google.com/file/d/1clXf_FaGEnJYHanPzRXbljLgsXVMFfIy/view?usp=sharing"
+                    href="https://drive.google.com/file/d/1dSibDcI9dPI-kP_9873YADqKQhfOud6V/view?usp=sharing"
                     target="_blank"
                     rel="noopener noreferrer"
                     className="border-dashed border-[#7B4BD3] border-2 rounded-full px-6 py-2 text-sm font-semibold text-[#7B4BD3] hover:bg-[#7B4BD3] hover:text-white transition-all duration-300 hover:cursor-pointer"
